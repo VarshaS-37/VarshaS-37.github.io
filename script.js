1543a072f673d54629405075086645e6bca3ca86
S Varsha <varshaadm2020@gmail.com>
2026-07-26 18:00:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log("Portfolio Loaded");
+const words = [
+
+    "Bioinformatics Graduate",
+
+    "Computational Biologist",
+
+    "AI for Healthcare"
+
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function type(){
+
+    if(charIndex < words[wordIndex].length){
+
+        typing.textContent += words[wordIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(type,80);
+
+    }
+
+    else{
+
+        setTimeout(erase,1800);
+
+    }
+
+}
+
+function erase(){
+
+    if(charIndex > 0){
+
+        typing.textContent = words[wordIndex].substring(0,charIndex-1);
+
+        charIndex--;
+
+        setTimeout(erase,40);
+
+    }
+
+    else{
+
+        wordIndex++;
+
+        if(wordIndex >= words.length){
+
+            wordIndex=0;
+
+        }
+
+        setTimeout(type,300);
+
+    }
+
+}
+
+type();
